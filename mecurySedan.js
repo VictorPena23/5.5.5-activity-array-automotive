8691e21fe8bece6792e2302ba28db05801cf43ac
VictorPena23 <halo_rider200219@yahoo.com>
2023-01-11 21:04:58 -0800

    //new changes

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,46 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+import { Vehicle } from "./vehicle.js";
 
-//this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+class car extends Vehicle {
+  constructor(make, model, year, color, mileage) {
+    super(make, model, year, color, mileage);
+    this.passenger = 0;
+    this.maximumPassengers = 5;
+    this.numberOfWheels = 4;
+    this.maximumSpeed = 160;
+    this.fuel = 10;
+    this.Service = false;
+  }
+  loadPassenger(num) {
+    // if passenger less than maximumPassengers then availableRoom == true
+    this.passenger = num;
+    if (this.passenger < this.maximumPassengers) {
+      console.log("There is available room for more passenger(s).");
+      return this.passenger;
+    } else {
+      console.log("Passenger capacity has been reached!");
+    }
+  }
+  start() {
+    // if fuel is greater than 0, then start == true
+    if (this.fuel > 0) {
+      console.log("Your vehicle engine has started!");
+      return this.started == true;
+    } else {
+      console.log("Please add fuel to your vehicle");
+    }
+  }
+  scheduleService(mileage) {
+    // if mileage is greater than 30000, time for maintenance == true
+    this.mileage = mileage;
+    if (this.mileage > 30000) {
+      console.log("It is time for vehicle maintenance!");
+      this.service = true;
+      return this.service;
+    } else {
+      console.log("Your vehicle is in good shape.");
+    }
+  }
+}
+
+// let myCar = new car("pontiac", "grand prix", "2001", "silver", 100000);
